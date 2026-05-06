b1fc7513052dbdc8a4c269d62534006489d69b55
bertinmick69 <bertinmick@yahoo.fr>
2026-05-06 13:44:53 +0200

    project.service.findbyid

--- src/services/project.service.js
+++ src/services/project.service.js
@@ -3,3 +3,11 @@ import * as projectModel from '../models/project.model.js';
 export const getAllProjects = async () => {
   return await projectModel.findAll();
 };
+
+export const getProjectById = async (id) => {
+  const project = await projectModel.findById(id);
+  if (!project) {
+    throw new AppError('project introuvable,404');
+  }
+  return project;
+}; 
