6b653d358ae8b3dde9b6b1f05215a1c5bb9a67f3
bertinmick69 <bertinmick@yahoo.fr>
2026-05-06 14:13:24 +0200

    Route projet par son id

--- src/controllers/project.controller.js
+++ src/controllers/project.controller.js
@@ -4,3 +4,9 @@ export const getAllProjects = async (req, res) => {
   const projects = await projectService.getAllProjects();
   res.json(projects);
 };
+
+export const getProjectById = async (req, res) => {
+  const { id } = req.params;
+  const project = await projectService.getProjectById(id);
+  res.json(project);
+};
