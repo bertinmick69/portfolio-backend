113fd682e5ada27cd3d04b8d88927cc97c36268e
bertinmick69 <bertinmick@yahoo.fr>
2026-05-06 21:56:03 +0200

    update project service

--- src/services/project.service.js
+++ src/services/project.service.js
@@ -20,3 +20,17 @@ export const create = async (data) => {
   }
   return project;
 };
+
+
+
+export const update = async (id, data) => {
+
+  const updatedProject = await projectModel.findById(id);
+  if (!updatedProject) {
+    throw new AppError('Erreur lors de la mise à jour du projet', 404);
+  }
+
+  const updated = await projectModel.update(id, data);
+  
+  return updated;
+};
