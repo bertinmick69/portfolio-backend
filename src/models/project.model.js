2866043c20d8277d052a770b10be036a60f4a137
bertinmick69 <bertinmick@yahoo.fr>
2026-05-06 14:40:59 +0200

    creat

--- src/models/project.model.js
+++ src/models/project.model.js
@@ -14,3 +14,9 @@ export const findById = async (id) => {
   }
   return rows[0]; 
 }
+
+export const create= async ({title, description, tech_stack, github_url, demo_url, image_url })=>{
+    const query = 'INSERT INTO projects (title, description, tech_stack, github_url, demo_url, image_url) VALUES (?, ?, ?, ?, ?, ?)';
+    const [rows] = await pool(query, [title, description, tech_stack, github_url, demo_url, image_url]);
+    return rows;    
+}
