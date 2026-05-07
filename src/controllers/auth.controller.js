7caa101ebc6ddb5b6a8ab60309df081267f3aae0
bertinmick69 <bertinmick@yahoo.fr>
2026-05-07 11:08:12 +0200

    finish

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,12 +1,3 @@
-// import { loginUser } from '../services/auth.service.js';
-
-// export const login = async (req, res, next) => {
-//   const { email, password } = req.body;
-
-//   const { token } = await loginUser({ email, password });
-
-//   res.json({ token });
-// };
 
 import { loginUser } from '../services/auth.service.js';
 
@@ -18,9 +9,9 @@ export const login = async (req, res, next) => {
 
     res.status(200).json({ 
       token,
-      user // Optionnel : il est souvent utile de renvoyer les infos de base de l'user
+      user
     });
   } catch (error) {
-    next(error); // Indispensable pour que ton middleware d'erreur attrape les mauvais identifiants
+    next(error); 
   }
 };
--- src/middlewares/authorize.middleware.js
+++ src/middlewares/authorize.middleware.js
@@ -2,7 +2,6 @@ import AppError from '../errors/AppError.js';
 
 export const authorize = (...roles) => {
   return (req, res, next) => {
-    // req.user est défini par le middleware authenticate
     if (!req.user || !roles.includes(req.user.role)) {
       return next(new AppError('Accès refusé', 403));
     }
